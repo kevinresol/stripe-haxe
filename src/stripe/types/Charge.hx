9bb14e387016a833176e8fb1463d477c7f51ac75
Kevin Leung <kevinresol@gmail.com>
2017-05-17 22:47:51 +0800

    formatting

--- src/stripe/types/Charge.hx
+++ src/stripe/types/Charge.hx
@@ -4,47 +4,47 @@ import tink.json.Value;
 
 typedef Charge = {
 	id:String,
-  object:String,
-  amount:Int,
-  amount_refunded:Int,
-  ?application:String,
-  ?application_fee:String,
-  balance_transaction:String,
-  captured:Bool,
-  created:Timestamp,
-  currency:String,
-  customer:String,
-  ?description:String,
-  ?destination:String,
-  ?dispute:String,
-  ?failure_code:String,
-  ?failure_message:String,
-  fraud_details:{
-	  ?user_report:String, // "safe" or "fraudulent"
-	  ?stripe_report:String, // "fraudulent"
-  },
-  ?invoice:String, // expandable Invoice
-  livemode:Bool,
-  metadata:Metadata,
-  ?on_behalf_of:String, // expandable Account
-  ?order:String, // expandable Order
-  outcome: {
-    network_status:String,
-    ?reason:String,
-    risk_level:String,
-    seller_message:String,
-    type:String,
-  },
-  paid:Bool,
-  ?receipt_email:String,
-  ?receipt_number:String,
-  refunded:Bool,
-  refunds:List,
-  ?review:String, // expandable Review
-  ?shipping:Shipping,
-  source:Object,
-  ?source_transfer:String, // expandable Transfer
-  ?statement_descriptor:String,
-  status:String,
-  ?transfer_group:String,
+	object:String,
+	amount:Int,
+	amount_refunded:Int,
+	?application:String,
+	?application_fee:String,
+	balance_transaction:String,
+	captured:Bool,
+	created:Timestamp,
+	currency:String,
+	customer:String,
+	?description:String,
+	?destination:String,
+	?dispute:String,
+	?failure_code:String,
+	?failure_message:String,
+	fraud_details:{
+		?user_report:String, // "safe" or "fraudulent"
+		?stripe_report:String, // "fraudulent"
+	},
+	?invoice:String, // expandable Invoice
+	livemode:Bool,
+	metadata:Metadata,
+	?on_behalf_of:String, // expandable Account
+	?order:String, // expandable Order
+	outcome: {
+		network_status:String,
+		?reason:String,
+		risk_level:String,
+		seller_message:String,
+		type:String,
+	},
+	paid:Bool,
+	?receipt_email:String,
+	?receipt_number:String,
+	refunded:Bool,
+	refunds:List,
+	?review:String, // expandable Review
+	?shipping:Shipping,
+	source:Object,
+	?source_transfer:String, // expandable Transfer
+	?statement_descriptor:String,
+	status:String,
+	?transfer_group:String,
 }
--- src/stripe/types/Customer.hx
+++ src/stripe/types/Customer.hx
@@ -3,19 +3,19 @@ package stripe.types;
 import haxe.DynamicAccess;
 
 typedef Customer = {
-  id: String,
-  account_balance: Int,
-  ?business_vat_id:String,
-  created:Timestamp,
-  ?currency:Currency,
-  ?default_source:String, // expandable
-  delinquent:Bool,
-  ?description:String,
-  ?discount:DynamicAccess<Discount>,
-  ?email:String,
-  livemode:Bool,
-  metadata:Metadata,
-  ?shipping:Shipping,
-  sources:List,
-  subscriptions:List,
+	id: String,
+	account_balance: Int,
+	?business_vat_id:String,
+	created:Timestamp,
+	?currency:Currency,
+	?default_source:String, // expandable
+	delinquent:Bool,
+	?description:String,
+	?discount:DynamicAccess<Discount>,
+	?email:String,
+	livemode:Bool,
+	metadata:Metadata,
+	?shipping:Shipping,
+	sources:List,
+	subscriptions:List,
 }
