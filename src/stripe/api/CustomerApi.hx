9921897e40d2d7ac16f5ebcadb27cb407fbad437
Kevin Leung <kevinresol@gmail.com>
2017-06-29 23:50:26 +0800

    Add update customer API

--- src/stripe/api/CustomerApi.hx
+++ src/stripe/api/CustomerApi.hx
@@ -10,4 +10,18 @@ interface CustomerApi {
 	@:delete('/')
 	public function delete():{deleted:Bool, id:String};
 	
+	@:post('/')
+	@:consumes('application/x-www-form-urlencoded')
+	public function update(body:{
+		?account_balance:Int,
+		?business_vat_id:String,
+		?coupon:String,
+		?default_source:String,
+		?description:String,
+		// ?metadata:Metadata,
+		?email:String,
+		// ?shipping:???,
+		?source:String,
+	}):Customer;
+	
 }
